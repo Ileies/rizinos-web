594ff6a4fd122705da03d0023f62f1307624d437
ileies <admin@ileies.de>
2026-06-27 18:55:59 +0200

    feat: load Google Analytics script in root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,25 @@
 	import Header from '$ui/homepage/Header.svelte';
 	import Footer from '$ui/homepage/Footer.svelte';
 	import { session, loadSession } from '$lib/session.svelte';
+	import { PUBLIC_GOOGLE_ANALYTICS_ID } from '$env/static/public';
 
 	let { children } = $props();
 
-	onMount(loadSession);
+	onMount(() => {
+		loadSession();
+		if (PUBLIC_GOOGLE_ANALYTICS_ID) {
+			const script = document.createElement('script');
+			script.async = true;
+			script.src = `https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GOOGLE_ANALYTICS_ID}`;
+			document.head.appendChild(script);
+			window.dataLayer = window.dataLayer ?? [];
+			function gtag(...args: unknown[]) {
+				window.dataLayer.push(args);
+			}
+			gtag('js', new Date());
+			gtag('config', PUBLIC_GOOGLE_ANALYTICS_ID);
+		}
+	});
 </script>
 
 <div class="flex min-h-screen flex-col">
