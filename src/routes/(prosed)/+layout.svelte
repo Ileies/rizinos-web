b93345d33bc4dc073d6f9c84e743414f5593eab8
ileies <admin@ileies.de>
2026-06-27 20:42:21 +0200

    fix(prosed): center legal pages by adding w-full to flex wrapper

--- src/routes/(prosed)/+layout.svelte
+++ src/routes/(prosed)/+layout.svelte
@@ -2,7 +2,7 @@
 	let { children } = $props();
 </script>
 
-<div class="m-8 flex justify-center">
+<div class="m-8 flex w-full justify-center">
 	<div class="prose prose-lg w-full max-w-4xl">
 		{@render children()}
 	</div>
